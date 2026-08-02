ad8e998db63cb20c2a280e265c1660d5a42ab8b1
שמואל <0534169095@xn--4dbjbascrao3i.com>
2026-08-02 06:24:53 +0300

    רענון מטמון לאחר תיקון הניהול

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "simchat-gallery-shell-v2-cloudflare";
+const CACHE_NAME = "simchat-gallery-shell-v3-admin-recovery";
 const APP_SHELL = ["./", "./index.html", "./cloudflare-client.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
